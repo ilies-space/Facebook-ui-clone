bd7810df636fa8fe6230e88522e28fba76fca773
ilies ouldmenouer <ilyasdzair1@gmail.com>
2021-09-20 16:13:23 +0100

    Initial commit

---
+++ Facebook ui clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook ui clone
+//
+//  Created by ilies on 20/9/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facebook ui clone/Facebook_ui_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_ui_cloneApp.swift
+//  Facebook ui clone
+//
+//  Created by ilies on 20/9/2021.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_ui_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
